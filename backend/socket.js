7cfbaea66e5b5c4ccdfe5d6da186de7ef55c8b34
ayaazworks <ayaazsid786@gmail.com>
2026-03-29 01:11:49 +0530

    Update CORS origin for socket server

--- backend/socket.js
+++ backend/socket.js
@@ -6,7 +6,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://friendly-grace-production-6569.up.railway.app",
         methods: ["GET", "POST"]
     }
 })
