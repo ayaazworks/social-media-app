71e8ab24d92df4aa8b6a853c6c9b0f9c8b3b24fb
ayaazworks <ayaazsid786@gmail.com>
2026-03-31 00:05:33 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,24 +3,12 @@ import LeftHome from '../components/LeftHome'
 import Feed from '../components/Feed'
 import RightHome from '../components/RightHome'
 import { useSelector } from 'react-redux'
-import  usegetSuggestedUsers  from '../hooks/usegetSuggestedUsers'
-import  usegetAllPost  from '../hooks/usegetAllPost'
-import  usegetAllLoops  from '../hooks/usegetAllLoops'
-import  usegetAllStories  from '../hooks/usegetAllStories'
-import  usegetFollowingList  from '../hooks/usegetFollowingList'
-import  usegetPrevChatUsers  from '../hooks/usegetPrevChatUsers'
-import usegetAllNotifications from '../hooks/usegetAllNotifications'
+
 
 
 const Home = () => {
 
-  usegetSuggestedUsers();
-  usegetAllPost();
-  usegetAllLoops();
-  usegetAllStories();
-  usegetFollowingList();
-  usegetPrevChatUsers();
-  usegetAllNotifications();
+  
   const { userData } = useSelector(state => state.user)
 
   // If userData isn't there yet, show a loading state instead of the components
