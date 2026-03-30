34fcc4681129b1c9ac5ec1649244f8f8cbbbf94a
ayaazworks <ayaazsid786@gmail.com>
2026-03-30 19:09:46 +0530

    Implement loading state in Home component
    Added loading state for user data retrieval.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,10 +2,23 @@ import React from 'react'
 import LeftHome from '../components/LeftHome'
 import Feed from '../components/Feed'
 import RightHome from '../components/RightHome'
+import { useSelector } from 'react-redux'
 
 const Home = () => {
+  // Get userData from store
+  const { userData } = useSelector(state => state.user)
+
+  // If userData isn't there yet, show a loading state instead of the components
+  if (!userData) {
+    return (
+      <div className='w-full h-screen flex justify-center items-center bg-[#0e1718]'>
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-white"></div>
+      </div>
+    )
+  }
+
   return (
-    <div className='w-full flex justify-center items-center'>
+    <div className='w-full flex justify-center items-start'>
       <LeftHome />
       <Feed />
       <RightHome />     
